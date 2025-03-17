c10a520bcaa25cc06ce5f36d18a8d2aaff7d5866
windy003 <928920471@qq.com>
2025-03-18 07:36:19 +0800

    再次添加了page.scroll.down

--- app.py
+++ app.py
@@ -87,6 +87,14 @@ def load_from_url(url):
         page.scroll.down()
         time.sleep(2)  # 等待图片加载
 
+        page.scroll.down()
+        time.sleep(2)  # 等待图片加载
+        page.scroll.down()
+        time.sleep(2)  # 等待图片加载
+
+        # 滚动页面以触发懒加载
+        page.scroll.down()
+        time.sleep(2)  # 等待图片加载
 
         imgs_A_Ds_lists = page.eles('xpath://div[@class="pgc-img"]')
 
