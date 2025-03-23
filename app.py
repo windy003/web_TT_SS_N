25a50f1d575a7f832dfe2dd58de3a6d86ad71791
windy003 <928920471@qq.com>
2025-03-23 13:46:53 +0800

    调整了代码,每个函数返回处都要加return

--- app.py
+++ app.py
@@ -139,18 +139,14 @@ def load_from_url(url):
             
             page.scroll.down()
             time.sleep(2)
-
-            
-
-            
-           
-           
         except Exception as e:
             print(f"向下翻页失败: {e}")
             traceback.print_exc()
+        
+        
+        
+        
         content=""
-        
-        
         # 获取文章标题
         try:
             title = page.ele('xpath://h1').text
@@ -187,62 +183,23 @@ def load_from_url(url):
             print(f"获取文章作者失败: {e}")
             traceback.print_exc()
 
-        # 获取文章内容
+        # 判断使用哪种模式
         try:
-            eles = page.eles('xpath://article/*')
-            if eles:
-                for ele in eles:
-                    if ele.tag == 'p':
-                        content += ele.text
-                    elif ele.tag == 'blockquote':
-                        content += ele.text
-                    elif ele.tag == 'div':
-                        content += ele.html
-                    elif ele.tag == 'img':
-                        content += ele.html
+            if len(page.eles('xpath://article/div/div/p')) > 5:
+                return mode_1(page,content,url)
+            elif len(page.eles('xpath://article/p')) > 5:
+                return mode_2(page,content,url)
+            else:
+                return wtt(page,content,url)
         except Exception as e:
-            print(f"获取文章内容失败: {e}")
-            traceback.print_exc()
-        # 获取微头条的发布时间和作者
-        try:
-            wtt_author=page.ele("xpath://div[@class='desc']/a").text  
-            if wtt_author:
-                content+= wtt_author + "<br>"
-        except Exception as e:
-            print(f"获取微头条作者失败: {e}")
-            traceback.print_exc()
-        try:
-            wtt_pub_time=page.ele("xpath://div[@class='desc']/p/span").text
-            if wtt_pub_time:
-                content+= wtt_pub_time + "<br><br>"
-        except Exception as e:
-            print(f"获取微头条发布时间失败: {e}")
+            print(f"判断使用哪种模式失败: {e}")
             traceback.print_exc()
 
-        # 微头条文本
-        try:
-            wtt = page.ele("xpath://div[@class='weitoutiao-html']")
-            if wtt:
-                content += wtt.text
-        except Exception as e:
-            print(f"获取微头条文本失败: {e}")
-            traceback.print_exc()   
 
 
-    
 
-        # 将 url 和 content 写入 backup.json 文件
-        try:
-            data = {
-                "url": url,
-                "content": content
-            }
-            with open("./backup.json", "w", encoding="utf-8") as f:
-                json.dump(data, f, ensure_ascii=False, indent=4)
-        except Exception as e:
-            print(f"写入 backup.json 文件失败: {e}")
-            traceback.print_exc()
-        return render_template('index.html',content=content)
+
+       
 
     except Exception as e:
         print(f"爬取过程中出现错误: {e}")
@@ -252,6 +209,111 @@ def load_from_url(url):
         page.quit()
         print("已关闭浏览器")
         traceback.print_exc()
+
+
+# 将 url 和 content 写入 backup.json 文件的函数
+def save_content(content,url):
+    try:
+        data = {
+            "url": url,
+            "content": content
+        }
+        with open("./backup.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f"写入 backup.json 文件失败: {e}")
+        traceback.print_exc()
+
+
+
+# 获取文章内容函数,形式1,示例:https://www.toutiao.com/article/7484780656023667211/?app=news_article&category_new=text_inner_flow&chn_id=94349612189&is_hit_share_recommend=0&req_id_new=20250323084928A9A596CD6AC5AA1F7C3B&share_did=MS4wLjACAAAAFIeCXqC-bpKxhp-FMV0EuKV-XQVB6-mSh701zwQO9RpTxDeXwV2yFdvhJIx5cc_q&share_token=188c7e83-3177-4243-a9dd-f688ccad8998&share_uid=MS4wLjABAAAASxKaOyZkLPeOekvLPbVvpEZxOI0hIbcOi5tNlEAvhVQNRKCRhGolFv7jwed4Tf1r&tt_from=copy_link&use_new_style=1&utm_campaign=client_share&utm_medium=toutiao_android&utm_source=copy_link&source=m_redirect
+def mode_1(page,content,url):
+    print("开始形式1")
+    try:
+        eles = page.eles('xpath://article/div/div/*')
+        if eles:
+            for ele in eles:
+                if ele.tag == 'p':
+                    if ele.text:
+                        content += ele.text
+                    elif ele.ele('xpath:.//img'):
+                        content += ele.ele('xpath:.//img').html
+                elif ele.tag == 'section':
+                    if ele.text:
+                        content += ele.text
+        print("用的是形式1")
+        save_content(content,url)
+        return render_template('index.html',content=content)
+        
+    except Exception as e:
+        print(f"获取文章,形式2,内容失败: {e}")
+        traceback.print_exc()
+
+
+
+
+
+# 获取文章内容,形式2
+# 示例:https://www.toutiao.com/article/7482967924794655271/?app=news_article&category_new=text_inner_flow&chn_id=94349612189&is_hit_share_recommend=0&req_id_new=20250322132314A2966E0FEC34CAD309F7&share_did=MS4wLjACAAAAaXbI3K95HwYTITAe_67APNaTonE8KefZ9UqB7B5wwMa_a0T5y1eus-kan8ClPqgH&share_token=ac1d72f6-ca61-49de-9533-a3213c2b6e68&share_uid=MS4wLjABAAAASxKaOyZkLPeOekvLPbVvpEZxOI0hIbcOi5tNlEAvhVQNRKCRhGolFv7jwed4Tf1r&tt_from=copy_link&use_new_style=1&utm_campaign=client_share&utm_medium=toutiao_android&utm_source=copy_link&source=m_redirect
+def mode_2(page,content,url):
+    print("开始形式2")
+    try:
+        eles = page.eles('xpath://article/*')
+        if eles:
+            try:
+                for ele in eles:
+                    if ele.tag == 'p':
+                        # 提取所有文本内容（包括嵌套标签中的文本）
+                        content += ele.text
+                    elif ele.tag == 'div':
+                        content += ele.html
+            except Exception as e:
+                print(f"获取文章内容失败: {e}")
+                traceback.print_exc()
+        print("用的是形式2")
+        save_content(content,url)
+        return render_template('index.html',content=content)
+    except Exception as e:
+        print(f"获取文章内容失败: {e}")
+        traceback.print_exc()
+
+
+
+
+
+# 获取微头条的发布时间和作者
+def wtt(page,content,url):
+    try:
+        wtt_author=page.ele("xpath://div[@class='desc']/a").text  
+        if wtt_author:
+            content+= wtt_author + "<br>"
+    except Exception as e:
+        print(f"获取微头条作者失败: {e}")
+        traceback.print_exc()
+    try:
+        wtt_pub_time=page.ele("xpath://div[@class='desc']/p/span").text
+        if wtt_pub_time:
+            content+= wtt_pub_time + "<br><br>"
+    except Exception as e:
+        print(f"获取微头条发布时间失败: {e}")
+        traceback.print_exc()
+
+    # 微头条文本
+    try:
+        wtt = page.ele("xpath://div[@class='weitoutiao-html']")
+        if wtt:
+            content += wtt.text
+    except Exception as e:
+        print(f"获取微头条文本失败: {e}")
+        traceback.print_exc()   
+
+    print("用的是微头条")
+    save_content(content,url)
+    return render_template('index.html',content=content)
+
+
+
+
 
 
 if __name__ == '__main__':
